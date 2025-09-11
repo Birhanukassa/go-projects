c11e2eeec358ff003bd3e380511f1537bbac50f4
birhanu kassa <artbirhanu@gmail.com>
2025-09-11 17:35:34 -0500

    refactor: organize project structure
    - Move all scripts to scripts/ directory
    - Move tools and installation files to tools/
    - Move Go source files to src/
    - Move all documentation to docs/
    - Clean root directory for better organization

--- example-main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-    fmt.Println("Hello, this is a runnable Go program!")
-    fmt.Println("The validation.go file is a library, not an executable.")
-}
